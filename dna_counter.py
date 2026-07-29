d0bc44be4c0f1db2d7074dca867adb3300af529c
jestergirl01-eng <jestergirl01@gmail.com>
2026-07-29 20:22:41 +0100

    Added user input and improved DNA analysis output
    Updated the DNA sequence input method to allow user input and converted it to uppercase.

--- dna_counter.py
+++ dna_counter.py
@@ -1,10 +1,13 @@
-dna = "ATGCGGTA"
+dna = input("Enter a DNA sequence: ")
+dna = dna.upper()
 
 a_count = dna.count("A")
 t_count = dna.count("T")
 g_count = dna.count("G")
 c_count = dna.count("C")
 
+print("DNA analysis")
+print("------------")
 print("A:", a_count)
 print("T:", t_count)
 print("G:", g_count)
