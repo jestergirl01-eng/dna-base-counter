f12a7e9fb8d32e2921f142b971986ccd76ed04db
jestergirl01-eng <jestergirl01@gmail.com>
2026-07-29 18:09:19 +0100

    Add DNA counter to count nucleotide occurrences

---
+++ dna_counter.py
@@ -0,0 +1,11 @@
+dna = "ATGCGGTA"
+
+a_count = dna.count("A")
+t_count = dna.count("T")
+g_count = dna.count("G")
+c_count = dna.count("C")
+
+print("A:", a_count)
+print("T:", t_count)
+print("G:", g_count)
+print("C:", c_count)
